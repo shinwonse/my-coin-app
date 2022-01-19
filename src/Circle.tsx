385e6067472ca52f2d3f7155f2f07aa9513d4044
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-01-19 21:54:12 +0900

    Initialize project using Create React App

---
+++ src/Circle.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+}
+
+interface ContainerProps {
+  bgColor: string;
+  borderColor: string;
+}
+
+const Container = styled.div<ContainerProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
+`;
+
+function Circle({ bgColor, borderColor }: CircleProps) {
+  return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor} />;
+}
+
+export default Circle;
